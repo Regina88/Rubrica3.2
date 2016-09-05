7c4316012db6f4e3038ddb20af91c8845825c58b
REGINA\reginamjr.14 <Regina88>
2016-09-04 21:27:51 -0500

    Login Usuario

--- Source/src/source/Source.java
+++ Source/src/source/Source.java
@@ -19,6 +19,7 @@ public class Source {
     public static void main(String[] args) {
            
     Scanner entrada = new Scanner (System.in);
+    Scanner mientrada = new Scanner (System.in);
     
         String clave = "12345";
         String usuario = "Regi";
@@ -26,7 +27,27 @@ public class Source {
         String miusuario = "";
         int bandera = 0;
         int contador = 0;
-        
+        do {
+            System.out.println("ingrese su nombre de usuario=> ");
+            miusuario = mientrada.nextLine();
+            System.out.println("ingrese su clave=>");
+            miclave = mientrada.nextLine();
+            contador = contador + 1;
+            if ((miclave.equals(clave)) && (miusuario.equals(usuario))) {
+                bandera = 1; // calve y nombre de usuario correcto
+            } else {
+                System.out.println("nombre de ususario no valido o clave no valida, intente nuevamente ");
+            }
+            if (contador == 3) {
+                bandera = 1; //llego al numero maximo
+            }
+        } while (bandera == 0);
+        if ((miclave.equals(clave)) && (miusuario.equals(usuario))) {
+            System.out.println("felizidades");
+        } else {
+            System.out.println("sistema bloqueado");
+        }  
+
         int numero1=0, numero2=0, numero3=0, menor=0;
         System.out.println("Ingrese el primer número  -> ");
         numero1=entrada.nextInt();
@@ -45,14 +66,10 @@ public class Source {
          System.out.printf("Entre %d, %d y %d el número menor es %d\n",numero1,numero2,numero3,menor);
        
     }
+  }
+  }
   
         
     
-}
-    
- 
-
-}
-    
-    
+   
 
