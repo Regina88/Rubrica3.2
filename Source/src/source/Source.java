0e20669c16214a45b747db4c708a1baa37048648
REGINA\reginamjr.14 <Regina88>
2016-09-04 19:55:28 -0500

    no message

---
+++ Source/src/source/Source.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reginamjr.14
+ */
+public class Source {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           
+    Scanner entrada = new Scanner (System.in);
+        int numero1=0, numero2=0, numero3=0, menor=0;
+        System.out.println("Ingrese el primer nÃºmero  -> ");
+        numero1=entrada.nextInt();
+        System.out.println("Ingresa el segundo nÃºmero  -> ");
+        numero2=entrada.nextInt();
+        System.out.println("Ingresa el tercer nÃºmero  -> ");
+        numero3=entrada.nextInt();
+
+    }
+    
+}
